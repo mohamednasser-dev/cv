f9aa1d698e13f0a2f61dc3a852d9c3efdc7ea5c6
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-11-29 14:44:46 +0200

    gg

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -902,10 +902,9 @@ class ProductController extends Controller
         }
 
 //        $pdf = PDF::loadView('webview.app_cv_design.default_design', ['data' => $data]);//    }else{
-//        $pdf = PDF::loadView('webview.app_cv_design.test_print', ['data' => $data,'lang'=>$request->lang]);
-        $pdf = PDF::loadView('webview.app_cv_design.first_design', ['data' => $data,'lang'=>$request->lang]);
+        $pdf = PDF::loadView('webview.app_cv_design.test_print', ['data' => $data,'lang'=>$request->lang]);
+//        $pdf = PDF::loadView('webview.app_cv_design.first_design', ['data' => $data,'lang'=>$request->lang]);
         $num = rand(00000,99999) .time();
-
         $pdf->save(public_path() .'/uploads/cvs/'.$num.'.pdf');
         $response = APIHelpers::createApiResponse(false, 200, '', '',  env('APP_URL')  .'/uploads/cvs/'.$num.'.pdf', $request->lang);
         return response()->json($response, 200);
