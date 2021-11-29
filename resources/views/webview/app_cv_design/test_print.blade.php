e9237bbd0e83e5a237d10a9f57fe56e4c3d886e4
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-11-29 14:59:14 +0200

    dsf

--- resources/views/webview/app_cv_design/test_print.blade.php
+++ resources/views/webview/app_cv_design/test_print.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    @dd('jjjj')
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -240,7 +241,7 @@
                     <div class="row">
 
                         <div class="col-md-11"> <h1>{{$data['personal_data']->full_name}}</h1></div>
-                       
+
                     </div>
 
                 </div>
