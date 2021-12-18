83a2e2c6655ec0cec87bf35977fc713ad26d35f4
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2021-12-18 09:40:43 +0200

    add personal data in model

--- app/cv.php
+++ app/cv.php
@@ -6,40 +6,55 @@ use Illuminate\Database\Eloquent\Model;
 
 class cv extends Model
 {
-    protected $fillable = ['design_number','user_id', 'deleted'];
+    protected $fillable = ['design_number', 'user_id', 'deleted'];
 
-    public function Personal_experience() {
-        return $this->hasOne('App\Cv_personal_experience', 'cv_id')->select('id','job_name','cv_id');
+    public function Personal_experience()
+    {
+        $pe = $this->hasOne('App\Cv_personal_experience', 'cv_id')->select('id', 'job_name', 'cv_id');
+        dd($this->hasOne('App\Cv_personal_experience', 'cv_id')->select('id', 'job_name', 'cv_id')->first());
+
+
+        return  $this->hasOne('App\Cv_personal_experience', 'cv_id')->select('id', 'job_name', 'cv_id');
+
     }
 
-    public function Personal_data() {
-        return $this->hasOne('App\cv_personal_data', 'cv_id')->select('id','full_name','image','cv_id','city_id')->with('City');
+    public function Personal_data()
+    {
+        return $this->hasOne('App\cv_personal_data', 'cv_id')->select('id', 'full_name', 'image', 'cv_id', 'city_id')->with('City');
     }
-    public function Personal_data_all() {
+
+    public function Personal_data_all()
+    {
         return $this->hasOne('App\cv_personal_data', 'cv_id');
     }
 
-    public function Certificats() {
+    public function Certificats()
+    {
         return $this->hasMany('App\Cv_certificat', 'cv_id');
     }
 
-    public function Courses() {
+    public function Courses()
+    {
         return $this->hasMany('App\Cv_course', 'cv_id');
     }
 
-    public function Design() {
+    public function Design()
+    {
         return $this->hasOne('App\Cv_design', 'cv_id');
     }
 
-    public function Hobbies() {
+    public function Hobbies()
+    {
         return $this->hasMany('App\Cv_hobby', 'cv_id');
     }
 
-    public function Job_experiences() {
+    public function Job_experiences()
+    {
         return $this->hasMany('App\Cv_job_experience', 'cv_id');
     }
 
-    public function Personal_experiences() {
+    public function Personal_experiences()
+    {
         return $this->hasMany('App\Cv_personal_experience', 'cv_id');
     }
 }
